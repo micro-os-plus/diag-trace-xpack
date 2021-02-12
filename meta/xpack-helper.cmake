06c02c868901e0895413593518fd8d182e4aa07f
Liviu Ionescu <ilg@livius.net>
2021-02-12 21:11:26 +0200

    add support for CMake find_package()

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -9,19 +9,25 @@
 #
 # -----------------------------------------------------------------------------
 
+if(micro-os-plus-diag-trace-included)
+  return()
+endif()
+
+set(micro-os-plus-diag-trace-included TRUE)
+
 message(STATUS "Including micro-os-plus-diag-trace...")
 
 # -----------------------------------------------------------------------------
 
 function(target_sources_micro_os_plus_diag_trace target)
 
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_sources(
     ${target}
 
-    PRIVATE
-      ${xpack_root_folder}/src/trace.cpp
+    PUBLIC
+      ${xpack_current_folder}/src/trace.cpp
   )
 
 endfunction()
@@ -30,14 +36,73 @@ endfunction()
 
 function(target_include_directories_micro_os_plus_diag_trace target)
 
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_include_directories(
     ${target}
 
     PUBLIC
-      ${xpack_root_folder}/include
+      ${xpack_current_folder}/include
   )
+
+endfunction()
+
+# -----------------------------------------------------------------------------
+
+function(target_compile_definitions_micro_os_plus_diag_trace target)
+
+  # None
+
+endfunction()
+
+# =============================================================================
+
+function(add_libraries_micro_os_plus_diag_trace)
+
+  # ---------------------------------------------------------------------------
+
+  if (NOT TARGET micro-os-plus::diag-trace)
+
+    add_library(micro-os-plus-diag-trace-objects OBJECT EXCLUDE_FROM_ALL)
+
+    target_sources_micro_os_plus_diag_trace(micro-os-plus-diag-trace-objects)
+    target_include_directories_micro_os_plus_diag_trace(micro-os-plus-diag-trace-objects)
+    target_compile_definitions_micro_os_plus_diag_trace(micro-os-plus-diag-trace-objects)
+
+    add_library(micro-os-plus::diag-trace ALIAS micro-os-plus-diag-trace-objects)
+    message(STATUS "micro-os-plus::diag-trace")
+
+    target_link_libraries(
+      micro-os-plus-diag-trace-objects
+
+      PUBLIC
+        micro-os-plus::common
+    )
+
+  endif()
+
+  # ---------------------------------------------------------------------------
+
+  if(NOT TARGET micro-os-plus-diag-trace-static)
+
+    add_library(micro-os-plus-diag-trace-static STATIC EXCLUDE_FROM_ALL)
+
+    target_sources_micro_os_plus_diag_trace(micro-os-plus-diag-trace-static)
+    target_include_directories_micro_os_plus_diag_trace(micro-os-plus-diag-trace-static)
+    target_compile_definitions_micro_os_plus_diag_trace(micro-os-plus-diag-trace-static)
+
+    add_library(micro-os-plus::diag-trace-static ALIAS micro-os-plus-diag-trace-static)
+
+    target_link_libraries(
+      micro-os-plus-diag-trace-static
+      
+      PUBLIC
+        micro-os-plus::common
+    )
+
+  endif()
+
+  # ---------------------------------------------------------------------------
 
 endfunction()
 
