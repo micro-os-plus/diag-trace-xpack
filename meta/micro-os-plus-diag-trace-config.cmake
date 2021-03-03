a580124cc527ba581082d37391ca69b476ffa1c3
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:32:32 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-diag-trace-config.cmake
+++ meta/micro-os-plus-diag-trace-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-diag-trace-included TRUE)
 
-message(STATUS "Including micro-os-plus-diag-trace...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
