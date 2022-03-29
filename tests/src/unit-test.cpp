d8faaa32014cd161e0431ad09316dd539aace23d
Liviu Ionescu <ilg@livius.net>
2022-03-29 22:12:00 +0300

    unit-test: update for expect() <<

--- tests/src/unit-test.cpp
+++ tests/src/unit-test.cpp
@@ -84,44 +84,44 @@ main (int argc, char* argv[])
   strcpy (buffer, "xxx");
 
   test_case ("Check trace::initialize", [] {
-    expect (eq (count, initial_count), "initial count");
-    expect (eq (buffer[0], 'x'), "initial content x");
+    expect (eq (count, initial_count)) << "initial count";
+    expect (eq (buffer[0], 'x')) << "initial content x";
 
     trace::initialize ();
-    expect (eq (count, 0), "count initialised");
-    expect (eq (buffer[0], '\0'), "initial content cleared");
+    expect (eq (count, 0)) << "count initialised";
+    expect (eq (buffer[0], '\0')) << "initial content cleared";
   });
 
   test_case ("Check trace::putchar", [] {
     int prev_count = count;
     trace::putchar ('c');
 
-    expect (eq ((count - prev_count), 1), "count increased by 1");
-    expect (eq (buffer[prev_count], 'c'), "buffer has c");
+    expect (eq ((count - prev_count), 1)) << "count increased by 1";
+    expect (eq (buffer[prev_count], 'c')) << "buffer has c";
   });
 
   test_case ("Check trace::puts", [] {
     int prev_count = count;
     trace::puts ("s");
 
-    expect (eq ((count - prev_count), 2), "count increased by 2");
-    expect (eq (buffer[prev_count], 's'), "buffer has s");
-    expect (eq (buffer[prev_count + 1], '\n'), "buffer has \\n");
+    expect (eq ((count - prev_count), 2)) << "count increased by 2";
+    expect (eq (buffer[prev_count], 's')) << "buffer has s";
+    expect (eq (buffer[prev_count + 1], '\n')) << "buffer has \\n";
   });
 
   test_case ("Check trace::printf", [] {
     int prev_count = count;
     trace::printf ("%s", "p");
 
-    expect (eq ((count - prev_count), 1), "count increased by 1");
-    expect (eq (buffer[prev_count], 'p'), "buffer has p");
+    expect (eq ((count - prev_count), 1)) << "count increased by 1";
+    expect (eq (buffer[prev_count], 'p')) << "buffer has p";
 
     prev_count = count;
     trace::printf ("%s\n", "q");
 
-    expect (eq ((count - prev_count), 2), "count increased by 2");
-    expect (eq (buffer[prev_count], 'q'), "buffer has q");
-    expect (eq (buffer[prev_count + 1], '\n'), "buffer has \\n");
+    expect (eq ((count - prev_count), 2)) << "count increased by 2";
+    expect (eq (buffer[prev_count], 'q')) << "buffer has q";
+    expect (eq (buffer[prev_count + 1], '\n')) << "buffer has \\n";
   });
 
   test_case ("Check trace::dump_args", [] {
@@ -136,10 +136,10 @@ main (int argc, char* argv[])
     std::string_view expected_main{
       "main(argc=3, argv=[\"n\", \"1\", \"2\"])\n"
     };
-    expect (eq ((count - prev_count), expected_main.length ()),
-            "count increased correctly");
-    expect (eq (std::string_view{ &buffer[prev_count] }, expected_main),
-            "buffer has main");
+    expect (eq ((count - prev_count), expected_main.length ()))
+        << "count increased correctly";
+    expect (eq (std::string_view{ &buffer[prev_count] }, expected_main))
+        << "buffer has main";
 
     prev_count = count;
     trace::dump_args (3, const_cast<char**> (argv_), "args");
@@ -147,59 +147,59 @@ main (int argc, char* argv[])
     std::string_view expected_args{
       "args(argc=3, argv=[\"n\", \"1\", \"2\"])\n"
     };
-    expect (eq ((count - prev_count), expected_args.length ()),
-            "count increased correctly");
-    expect (eq (std::string_view{ &buffer[prev_count] }, expected_args),
-            "buffer has main");
+    expect (eq ((count - prev_count), expected_args.length ()))
+        << "count increased correctly";
+    expect (eq (std::string_view{ &buffer[prev_count] }, expected_args))
+        << "buffer has main";
   });
 
   test_case ("Check trace::flush", [] {
     trace::flush ();
-    expect (eq (buffer[count], flush_mark), "flush mark found");
+    expect (eq (buffer[count], flush_mark)) << "flush mark found";
   });
 
   count = initial_count;
   strcpy (buffer, "xxx");
 
   test_case ("Check micro_os_plus_trace_initialize", [] {
-    expect (eq (count, initial_count), "initial count");
-    expect (eq (buffer[0], 'x'), "initial content x");
+    expect (eq (count, initial_count)) << "initial count";
+    expect (eq (buffer[0], 'x')) << "initial content x";
 
     micro_os_plus_trace_initialize ();
-    expect (eq (count, 0), "count initialised");
-    expect (eq (buffer[0], '\0'), "initial content cleared");
+    expect (eq (count, 0)) << "count initialised";
+    expect (eq (buffer[0], '\0')) << "initial content cleared";
   });
 
   test_case ("Check micro_os_plus_trace_putchar", [] {
     int prev_count = count;
     micro_os_plus_trace_putchar ('c');
 
-    expect (eq ((count - prev_count), 1), "count increased by 1");
-    expect (eq (buffer[prev_count], 'c'), "buffer has c");
+    expect (eq ((count - prev_count), 1)) << "count increased by 1";
+    expect (eq (buffer[prev_count], 'c')) << "buffer has c";
   });
 
   test_case ("Check micro_os_plus_trace_puts", [] {
     int prev_count = count;
     micro_os_plus_trace_puts ("s");
 
-    expect (eq ((count - prev_count), 2), "count increased by 2");
-    expect (eq (buffer[prev_count], 's'), "buffer has s");
-    expect (eq (buffer[prev_count + 1], '\n'), "buffer has \\n");
+    expect (eq ((count - prev_count), 2)) << "count increased by 2";
+    expect (eq (buffer[prev_count], 's')) << "buffer has s";
+    expect (eq (buffer[prev_count + 1], '\n')) << "buffer has \\n";
   });
 
   test_case ("Check micro_os_plus_trace_printf", [] {
     int prev_count = count;
     micro_os_plus_trace_printf ("%s", "p");
 
-    expect (eq ((count - prev_count), 1), "count increased by 1");
-    expect (eq (buffer[prev_count], 'p'), "buffer has p");
+    expect (eq ((count - prev_count), 1)) << "count increased by 1";
+    expect (eq (buffer[prev_count], 'p')) << "buffer has p";
 
     prev_count = count;
     micro_os_plus_trace_printf ("%s\n", "q");
 
-    expect (eq ((count - prev_count), 2), "count increased by 2");
-    expect (eq (buffer[prev_count], 'q'), "buffer has q");
-    expect (eq (buffer[prev_count + 1], '\n'), "buffer has \\n");
+    expect (eq ((count - prev_count), 2)) << "count increased by 2";
+    expect (eq (buffer[prev_count], 'q')) << "buffer has q";
+    expect (eq (buffer[prev_count + 1], '\n')) << "buffer has \\n";
   });
 
   test_case ("Check micro_os_plus_trace_dump_args", [] {
@@ -214,10 +214,10 @@ main (int argc, char* argv[])
     std::string_view expected_main{
       "main(argc=3, argv=[\"n\", \"1\", \"2\"])\n"
     };
-    expect (eq ((count - prev_count), expected_main.length ()),
-            "count increased correctly");
-    expect (eq (std::string_view{ &buffer[prev_count] }, expected_main),
-            "buffer has main");
+    expect (eq ((count - prev_count), expected_main.length ()))
+        << "count increased correctly";
+    expect (eq (std::string_view{ &buffer[prev_count] }, expected_main))
+        << "buffer has main";
 
     prev_count = count;
     trace::dump_args (3, const_cast<char**> (argv_), "args");
@@ -225,15 +225,15 @@ main (int argc, char* argv[])
     std::string_view expected_args{
       "args(argc=3, argv=[\"n\", \"1\", \"2\"])\n"
     };
-    expect (eq ((count - prev_count), expected_args.length ()),
-            "count increased correctly");
-    expect (eq (std::string_view{ &buffer[prev_count] }, expected_args),
-            "buffer has main");
+    expect (eq ((count - prev_count), expected_args.length ()))
+        << "count increased correctly";
+    expect (eq (std::string_view{ &buffer[prev_count] }, expected_args))
+        << "buffer has main";
   });
 
   test_case ("Check micro_os_plus_trace_flush", [] {
     micro_os_plus_trace_flush ();
-    expect (eq (buffer[count], flush_mark), "flush mark found");
+    expect (eq (buffer[count], flush_mark)) << "flush mark found";
   });
 
   return exit_code ();
