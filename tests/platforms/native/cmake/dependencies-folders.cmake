833c4c2ed0f5814e7e5f639c6cc393ae09abce32
Liviu Ionescu <ilg@livius.net>
2025-03-20 14:05:29 +0200

    tests: platforms/native updates

---
+++ tests/platforms/native/cmake/dependencies-folders.cmake
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------------------
+# DO NOT EDIT!
+# Automatically generated from build-helper/templates.
+#
+# This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+# Copyright (c) 2022 Liviu Ionescu. All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/mit.
+#
+# -----------------------------------------------------------------------------
+
+# Define a list of folders where the platform dependencies are located.
+
+# -----------------------------------------------------------------------------
+
+# Informative.
+set(xpack_device_compile_definition "MICRO_OS_PLUS_DEVICE_NATIVE")
+
+set(xpack_platform_compile_definition "MICRO_OS_PLUS_PLATFORM_NATIVE")
+
+# -----------------------------------------------------------------------------
+set(xpack_dependencies_folders
+
+  # The BINARY_DIR is the `build/<config>` folder.
+  "${CMAKE_BINARY_DIR}/xpacks/@micro-os-plus/architecture-synthetic-posix"
+
+  # The SOURCE_DIR is the `tests` folder.
+  # "${CMAKE_SOURCE_DIR}/xpacks/@micro-os-plus/diag-trace"
+  "${CMAKE_SOURCE_DIR}/xpacks/@micro-os-plus/micro-test-plus"
+)
+
+# -----------------------------------------------------------------------------
---
+++ tests/platforms/native/include/micro-os-plus/platform.h
@@ -0,0 +1,28 @@
+/*
+ * DO NOT EDIT!
+ * Automatically generated from build-helper/templates.
+ *
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * Copyright (c) 2017 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/mit.
+ */
+
+#ifndef MICRO_OS_PLUS_PLATFORM_H_
+#define MICRO_OS_PLUS_PLATFORM_H_
+
+#include <micro-os-plus/device.h>
+
+// ----------------------------------------------------------------------------
+
+// ...
+
+// ----------------------------------------------------------------------------
+
+#endif /* MICRO_OS_PLUS_PLATFORM_H_ */
+
+// ----------------------------------------------------------------------------
