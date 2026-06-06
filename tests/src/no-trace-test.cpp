4e8b0625db0453caa15484b482864893156d6a34
Liviu Ionescu <ilg@livius.net>
2026-06-06 21:27:01 +0300

    use __has_include(<micro-os-plus/config.h>)

--- tests/src/no-trace-test.cpp
+++ tests/src/no-trace-test.cpp
@@ -16,9 +16,9 @@
  * function.
  */
 
-#if defined(MICRO_OS_PLUS_INCLUDE_CONFIG_H)
+#if __has_include(<micro-os-plus/config.h>)
 #include <micro-os-plus/config.h>
-#endif // MICRO_OS_PLUS_INCLUDE_CONFIG_H
+#endif // __has_include(<micro-os-plus/config.h>)
 
 #if !defined(MICRO_OS_PLUS_TRACE)
 #error "MICRO_OS_PLUS_TRACE should be defined"
